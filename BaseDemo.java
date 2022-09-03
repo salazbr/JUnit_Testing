ba558989f52753469a542eddc3b09fe83c7b3b11
BRIAN ALLYN SALAZAR (DOCU-IRIS-ISD-OOCLL/MNL) <SALAZBR@OOCL.COM>
2022-09-04 03:25:08 +0800

    first commit

---
+++ BaseDemo.java
@@ -0,0 +1,12 @@
+package demo.base;
+
+public class BaseDemo {
+    public String print() {
+        return "Hello World";
+    }
+
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+}
